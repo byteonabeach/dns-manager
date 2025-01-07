85e42ef70c8df0addb22f48f4fe8c850a2cc7781
OxFF <oxff@noreply.localhost>
2025-01-08 00:52:33 +0300

    Update dns_manager.go

--- dns_manager.go
+++ dns_manager.go
@@ -2,38 +2,38 @@ package dns_manager
 
 type RecordType int
 
-const ( 
-  None RecordType = iota
-  A
-  AAAA
-  MX
-  CNAME
-  TXT
+const (
+	None RecordType = iota
+	A
+	AAAA
+	MX
+	CNAME
+	TXT
 )
 
 type Record struct {
-  Type RecordType
-  Host string
-  Content string
-  TTL uint16
+	Type    RecordType
+	Host    string
+	Content string
+	TTL     uint16
 }
 
-type actions interface {
-  AddRecord(Record) Response 
-  DeleteRecord() Response
-  UpdateRecord(Record) Response
-  ReadRecords() (error, *[]Record)
+type Actions interface {
+	AddRecord(Record) Response
+	DeleteRecord() Response
+	UpdateRecord(Record) Response
+	ReadRecords() (error, *[]Record)
 }
 
 type manager interface {
-  New() *actions
+	New() Actions
 }
 
 type Response struct {
-  error string
-  message string
+	error   string
+	message string
 }
 
-func New(manager_config manager) *actions {
-  return manager_config.New() 
+func New(manager_config manager) Actions {
+	return manager_config.New()
 }
